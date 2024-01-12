a00f361bf0a0f04033316cc31c4f22587858f945
simrett <leesim242@gmail.com>
2024-01-12 11:07:26 -0800

    some modifications

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import uuid
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,12 +8,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-port = 5000
-host = '0.0.0.0'
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -51,4 +47,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=5000)
